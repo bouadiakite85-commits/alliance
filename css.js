e2d4ab7229fd27b3c503b51850f82e528d16faff
bouadiakite85-commits <bouadiakite85@gmail.com>
2026-05-23 18:29:42 +0200

    Create css.js

---
+++ css.js
@@ -0,0 +1,4 @@
+// Exemple d'une carte Glassmorphism pour le lecteur Radio
+<div className="bg-white/10 backdrop-blur-md border border-white/20 p-6 rounded-2xl shadow-xl">
+   <h2 className="text-white font-bold">📻 ARM Radio</h2>
+</div>
